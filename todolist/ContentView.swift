782fedd86edca0811a0f9a4003816c45c05523ca
mollysweeney <mollygirl1232@gmail.com>
2025-05-02 22:13:55 -0500

    finished app

--- todolist/ContentView.swift
+++ todolist/ContentView.swift
@@ -1,43 +1,48 @@
-//
-//  ContentView.swift
-//  todolist
-//
-//  Created by Molly Sweeney on 5/2/25.
-//
-
 import SwiftUI
 
-
 struct ContentView: View {
-    @State
-    private var reminders = Reminder.samples
-    
-    @State
-    private var isAddRemindereDialogPresented = false
-    
+    @State private var reminders = Reminder.samples
+    @State private var isAddReminderDialogPresented = false
+
     private func presentAddReminderView() {
         isAddReminderDialogPresented.toggle()
     }
-    
+
     var body: some View {
         List($reminders) { $reminder in
             HStack {
-                Image(systemName: reminder.isCompleted
-                   ? "largecircle.fill.circle"
-                      : "circle")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-                .onTapGesture {
-                    reminder.isCompleted.toggle()
-                }
+                Image(systemName: reminder.isCompleted ? "largecircle.fill.circle" : "circle")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                    .onTapGesture {
+                        reminder.isCompleted.toggle()
+                    }
                 Text(reminder.title)
             }
+        }
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomBar) {
+                Button(action: presentAddReminderView) {
+                    HStack {
+                        Image(systemName: "plus.circle.fill")
+                        Text("New Reminder")
+                    }
+                }
+                Spacer()
             }
         }
-        //.padding()
+        .sheet(isPresented: $isAddReminderDialogPresented) {
+            AddReminderView { reminder in
+                reminders.append(reminder)
+            }
+        }
     }
-
+}
 
 #Preview {
-    ContentView()
+    NavigationStack {
+        ContentView()
+            .navigationTitle("Reminders")
+    }
 }
+
