7f480611f63216a2267f80f69c06097790a34754
mollysweeney <mollygirl1232@gmail.com>
2025-05-02 21:12:21 -0500

    Initial Commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Molly Sweeney on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
